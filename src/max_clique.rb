fa567dd7520b0b62d830fb40778a36331c500e4e
Ian <ian@linkportfolio.net>
2012-06-01 14:55:19 -0700

    documentation and cleanup

--- src/max_clique.rb
+++ src/max_clique.rb
@@ -1,21 +1,39 @@
+# @function max_clique(chosen, graph)
+# Finds the maximum clique in a graph. This uses a highly unoptimized brute-force method. This only works if you are passing in the node with the largest number of connections.
+# There are several steps:
+#   1. Visit every node within the graph
+#   2. Calculate: visited node's connections ∩ chosen node's connections
+#       a) These values are stored in a hash of arrays, indexed by the visited vertex's number
+#   3. Sort the previously stored hash by largest number of mutual connections.
+#   4. For each mutual connection, add it to the clique IFF it is a superset of the nodes within clique
+#       a) Otherwise, you would wind up with nodes that are not mutually connected!
+#
+# @param graph
+# A hash of arrays, keyed by vertex number. EG:
+#   "1" => [0,1,3]
+# The above would represent vert 1 and its connections to verts 0, 1 and 3.
+#
+# @param chosen
+# The number of the selected
+
 def max_clique(chosen, graph)
-  print "passed graph: ", chosen, "\t", graph, "\n"
+  #print "passed graph: ", chosen, "\t", graph, "\n"
   return [] if graph[chosen].nil?
-  # find biggest clique:
-  @clique = [].to_set
 
+  @clique = Set.new
   intersections = {}
-
   chosen_set = graph[chosen].to_set
 
   print "chosen set: ", chosen_set.inspect, "\n"
   graph[chosen].each do |potential|
     # this set conversion is more convenient, but probably not very fast
-    # lets us do easy intersection calculations
+    # lets us do easy intersection calculations (such as intersection)
     potential_set = graph[Integer(potential)] # this is a really awkward way of doing this..
     print 'potential set: ', potential_set, "\n"
 
     # collect all intersections so we can sort by size
+    # this represents the number of connections the chosen node shares with the visited node
+    # the more connections, the better the candidate for a clique
     unless potential_set.nil?
       intersections[potential] = chosen_set.intersection(potential_set).to_a
     end
