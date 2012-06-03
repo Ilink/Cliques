d4119c92aa5105d95a6e6233bbf08be03c818137
Ian Link <ian@linkportfolio.net>
2012-06-02 19:50:54 -0700

    removed extra debug info

--- src/max_clique.rb
+++ src/max_clique.rb
@@ -23,16 +23,16 @@ def max_clique(chosen, graph)
   #print "passed graph: ", chosen, "\t", graph, "\n"
   return [] if graph[chosen].nil?
 
-  @clique = Set.new
+  clique = Set.new
   intersections = {}
   chosen_set = graph[chosen].to_set
 
-  print "chosen set: ", chosen_set.inspect, "\n"
+  #print "chosen set: ", chosen_set.inspect, "\n"
   graph[chosen].each do |potential|
     # this set conversion is more convenient, but probably not very fast
     # lets us do easy intersection calculations (such as intersection)
     potential_set = graph[Integer(potential)] # this is a really awkward way of doing this..
-    print 'potential set: ', potential_set, "\n"
+    #print 'potential set: ', potential_set, "\n"
 
     # collect all intersections so we can sort by size
     # this represents the number of connections the chosen node shares with the visited node
@@ -46,18 +46,18 @@ def max_clique(chosen, graph)
     y.size <=> x.size
   }
   intersections.reverse! # I have NO idea why doing x.size <=> y.size doesn't revers the values, but it doesn't
-  print 'intersections: ', intersections, "\n"
+  #print 'intersections: ', intersections, "\n"
 
   intersections.each do |intersection|
     intersection_set = intersection[1].to_set # inefficient!
-    print "candidate: ", intersection_set.inspect, "\tclique: ", @clique.inspect, "\n"
-    if intersection_set.intersection(@clique) == @clique
-      @clique.add(intersection[0])
+    #print "candidate: ", intersection_set.inspect, "\tclique: ", @clique.inspect, "\n"
+    if intersection_set.intersection(clique) == clique
+      clique.add(intersection[0])
     end
   end
 
-  @clique.add(chosen.to_s) # add the selected node itself
-  @clique.to_a
+  clique.add(chosen.to_s) # add the selected node itself
+  clique.to_a
 end
 
 def remove_keys(arr, hash)
@@ -74,25 +74,27 @@ end
 #   2. Remove that clique's verts from the graph
 #   3. Repeat until all verts have been removed
 #
-# This idea rests on the assumption that having a lot of connections means greater potential for a large clique. I'm not entirely sure if this is correct!
-# TODO: implement sorting, so we start with the clique with the most number of connections. Right now, the test input is already sorted like this, so it doesn't matter for testing
+# Something is wrong and the order of the input matters. I don't know why. Is there a side effect from the max_clique function?
 
 def all_max_cliques(_graph)
   cliques = []
   graph = _graph
 
-  until graph.empty?
+  #until graph.empty?
     largest_clique = []
     graph.each do |vert|
       clique = max_clique(vert[0], graph)
       print 'cliqueeee', clique, "\t", vert, "\n"
-      largest_clique = clique if clique.size > largest_clique.size
+      if clique.size > largest_clique.size
+        #print "largest clique was: ", largest_clique, "\t New clique: ", clique, "\n"
+        largest_clique = clique
+      end
     end
     cliques.push(largest_clique)
     print "largest clique!: ", largest_clique, "\n"
 
-    graph = remove_keys(largest_clique, graph)
-  end
+    #graph = remove_keys(largest_clique, graph)
+  #end
   print cliques, "\n"
 
 end
